6b580154f6401033d4df93f555a13397023181e6
dinaamagdy <magdyd@codescalers.com>
2019-10-27 20:02:01 +0200

    add tests

---
+++ tests/Elements.py
@@ -0,0 +1,11 @@
+elements = {
+    "Jobs_bage": ["XPATH" '//*[@id="navbarText"]/ul/li[2]/a/span'],
+    "jobs_table": ["XPATH", '//*[@id="sapper"]/main/div/div/div/div[3]/div/div/table'],
+    "summary_table": ["XPATH", '//*[@id="sapper"]/main/div/div/div/div[2]/div/table'],
+    "ALL_filter": ["XPATH", "//button[contains(.,'All')]"],
+    "success_filter": ["XPATH", "//button[contains(.,'Success')]"],
+    "failure_filter": ["XPATH", "//button[contains(.,'Failure')]"],
+    "New_filter": ["XPATH", "//button[contains(.,'New')]"],
+    "Running_filter": ["XPATH", "//button[contains(.,'Running')]"],
+    "Halted_filter": ["XPATH", "//button[contains(.,'Halted')]"],
+}
--- tests/base_test.py
+++ tests/base_test.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from testconfig import config
 import unittest, time
 from loguru import logger
+from Elements import elements
+from selenium.webdriver.common.by import By
 
 
 class BaseTest(unittest.TestCase):
@@ -28,7 +30,12 @@ class BaseTest(unittest.TestCase):
 
     def set_browser(self):
         if self.browser == "chrome":
-            self.driver = webdriver.Chrome()
+            options = webdriver.ChromeOptions()
+            options.add_argument("--no-sandbox")
+            options.add_argument("--headless")
+            options.add_argument("--disable-dev-shm-usage")
+            self.driver = webdriver.Chrome(options)
+
         elif self.browser == "firefox":
             self.driver = webdriver.Firefox()
         else:
@@ -48,6 +55,31 @@ class BaseTest(unittest.TestCase):
         screen_size = screen_dimention["width"] * screen_dimention["height"]
         if screen_size < 1800 * 1000:
             self.driver.set_window_size(1800, 1000)
+
+    def find_element(self, element):
+        method = elements[element][0]
+        value = elements[element][1]
+        element_value = self.driver.find_element(getattr(By, method), value)
+        return element_value
+
+    def get_table_head_elements(self, element):
+        table = self.find_element(element)
+        thead = table.find_elements_by_tag_name("thead")
+        thead_row = thead[0].find_elements_by_tag_name("tr")
+        return thead_row[0].find_elements_by_tag_name("th")
+
+    def get_table_rows(self, element=None):
+
+        element = self.find_element(element)
+        tbody = element.find_element_by_tag_name("tbody")
+        rows = tbody.find_elements_by_tag_name("tr")
+        return rows
+
+    def get_table_row(self, table, i):
+        table_row = self.get_table_rows(table["data"])[i]
+        row_cells = table_row.find_elements_by_tag_name("td")
+        self.assertTrue(row_cells)
+        return [x.text for x in row_cells]
 
     @staticmethod
     def info(message):
