b4a52ad3eda71747f36ae2c93da54ad2abfceb8e
dinaamagdy <magdyd@codescalers.com>
2019-10-30 10:22:01 +0200

    add more tests

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from testconfig import config
-import unittest, time
+import unittest, time, uuid
 from loguru import logger
 from Elements import elements
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BaseTest(unittest.TestCase):
     LOGGER = logger
-    LOGGER.add("apps_{time}.log")
+    LOGGER.add("myjobs_{time}.log")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,8 +20,6 @@ class BaseTest(unittest.TestCase):
         self.myjobs_page = self.url + "/myjobs"
         self.blog = self.url + "blog"
         self.pastebin = self.url + "pastebin"
-
-    def setUp(self):
         self._testID = self._testMethodName
         self._startTime = time.time()
         self.set_browser()
@@ -34,7 +33,7 @@ class BaseTest(unittest.TestCase):
             options.add_argument("--no-sandbox")
             options.add_argument("--headless")
             options.add_argument("--disable-dev-shm-usage")
-            self.driver = webdriver.Chrome(options)
+            self.driver = webdriver.Chrome(options=options)
 
         elif self.browser == "firefox":
             self.driver = webdriver.Firefox()
@@ -62,11 +61,12 @@ class BaseTest(unittest.TestCase):
         element_value = self.driver.find_element(getattr(By, method), value)
         return element_value
 
-    def get_table_head_elements(self, element):
+    def get_table_head_data(self, element):
         table = self.find_element(element)
         thead = table.find_elements_by_tag_name("thead")
         thead_row = thead[0].find_elements_by_tag_name("tr")
-        return thead_row[0].find_elements_by_tag_name("th")
+        head_elements = thead_row[0].find_elements_by_tag_name("th")
+        return [x.text for x in head_elements]
 
     def get_table_rows(self, element=None):
 
@@ -75,12 +75,28 @@ class BaseTest(unittest.TestCase):
         rows = tbody.find_elements_by_tag_name("tr")
         return rows
 
-    def get_table_row(self, table, i):
-        table_row = self.get_table_rows(table["data"])[i]
+    def get_table_row(self, element, i):
+        table_row = self.get_table_rows(element)[i]
         row_cells = table_row.find_elements_by_tag_name("td")
         self.assertTrue(row_cells)
         return [x.text for x in row_cells]
 
+    def wait_until_element_located(self, element):
+        method = elements[element][0]
+        value = elements[element][1]
+        for _ in range(3):
+            try:
+                self.wait.until(EC.visibility_of_element_located((getattr(By, method), value)))
+                return True
+            except:
+                time.sleep(1)
+        else:
+            return False
+
     @staticmethod
     def info(message):
         BaseTest.LOGGER.info(message)
+
+    @staticmethod
+    def rand_string(size=10):
+        return str(uuid.uuid4()).replace("-", "")[1:10]
